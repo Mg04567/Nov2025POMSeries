b6d396de37382f1c7764437ea6f393ae4f6ce073
Mg04567 <shilpamg04@gmail.com>
2026-04-08 01:17:23 +0530

    Update MyPage.java

--- src/main/java/com/opencart/qa/pages/MyPage.java
+++ src/main/java/com/opencart/qa/pages/MyPage.java
@@ -2,10 +2,13 @@ package com.opencart.qa.pages;
 
 public class MyPage {
 
-int age=10;
-String name="Tom";
 
-private void syso() {
-	System.out.println("hello page");
+String browser="chrome";
+
+public void login() {
+	System.out.println("Hello login");
+	System.out.println("Hello login");
+	System.out.println("Hello login");
+	System.out.println("Hello login");
 }
 }
