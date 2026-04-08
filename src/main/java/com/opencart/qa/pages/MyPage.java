0395b73131745f7038913e0d2b3245ebb8177908
unknown <shilpamg04@gmail.com>
2026-04-08 11:44:16 +0530

    updated my page

--- src/main/java/com/opencart/qa/pages/MyPage.java
+++ src/main/java/com/opencart/qa/pages/MyPage.java
@@ -2,20 +2,8 @@ package com.opencart.qa.pages;
 
 public class MyPage {
 
-int age=10;
-
-String name="Tom";
-private void syso() {
-	System.out.println("hello ");
-}
 
 
-public void login() {
-	System.out.println("Hello app");
-	System.out.println("Hello app");
-	System.out.println("Hello app");
-	System.out.println("Hello app");
-}
 
 public void login() {
 	System.out.println("Hello login");
@@ -23,4 +11,6 @@ public void login() {
 	System.out.println("Hello login");
 	System.out.println("Hello login");
 }
+
+
 }
