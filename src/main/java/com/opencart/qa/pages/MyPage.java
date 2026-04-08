db425d7df635f30be19d96b8bf8cf888b7f9c254
Mg04567 <shilpamg04@gmail.com>
2026-04-08 11:43:40 +0530

    Update MyPage.java

--- src/main/java/com/opencart/qa/pages/MyPage.java
+++ src/main/java/com/opencart/qa/pages/MyPage.java
@@ -2,13 +2,7 @@ package com.opencart.qa.pages;
 
 public class MyPage {
 
-
-String browser="chrome";
-
-public void login() {
-	System.out.println("Hello app");
-	System.out.println("Hello app");
-	System.out.println("Hello app");
-	System.out.println("Hello app");
+public void logout() {
+	System.out.println("Hello logout");
 }
 }
